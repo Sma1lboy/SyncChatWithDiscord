71c2541d5deeaf32abff194167e8ecec1b33d9dc
Sma1lboy <541898146chen@gmail.com>
2022-11-16 19:19:35 -0500

    implemented some logic to userinfo

--- src/main/java/me/sma1lboy/syncchatwithdiscord/jda/commands/UserInfoImpl.java
+++ src/main/java/me/sma1lboy/syncchatwithdiscord/jda/commands/UserInfoImpl.java
@@ -1,5 +1,8 @@
 package me.sma1lboy.syncchatwithdiscord.jda.commands;
 
+import me.sma1lboy.syncchatwithdiscord.db.pojo.User;
+import me.sma1lboy.syncchatwithdiscord.db.service.UserService;
+import me.sma1lboy.syncchatwithdiscord.db.service.impl.UserServiceImpl;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -11,6 +14,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  */
 public class UserInfoImpl extends ListenerAdapter {
 
+    UserService userService = new UserServiceImpl();
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent e)
     {
@@ -19,15 +23,24 @@ public class UserInfoImpl extends ListenerAdapter {
          * starting create the embed
          */
         EmbedBuilder builder = new EmbedBuilder();
-        builder.addField("Username", "Sma1lboy#5210", true);
-        builder.addField("Minecraft Nickname", "Sma1lboy", true);
+
+        User user = userService.getUser(e.getUser().getAsTag(), e.getUser().getId());
+        String dcUsername = user.getDcId();
+        String mcUsername = user.getMcId() == null ? "Not Assign yet" : user.getMcId();
+        Integer dayTrack = user.getDailyTrack() == null ? 0 : user.getDailyTrack();
+        /*
+        TODO status check with different level strick daily tracking
+         */
+        builder.addField("Username", dcUsername, true);
+        builder.addField("Minecraft Nickname", mcUsername, true);
         builder.addField("Paste Two Weeks in Game", "1 day 2hrs 🔥", true);
-        builder.addField("Daily Check Track", "128 days 🔥", false);
+        builder.addField("Daily Check Track", dayTrack + " days 🔥", false);
         //TODO Change to local img
         builder.setThumbnail("https://play-lh.googleusercontent.com/DJexP6PO8dL06XvNrjG7plb7SW_SaxuNamO80ab512JA71lBEBUnaJCaZzlqWVrrlEiG=w240-h480-rw");
         builder.setAuthor("@someone");
         //TODO add footer icon
         builder.setFooter("@MCMsgBot");
+        //TODO check button
         e.replyEmbeds(builder.build()).setEphemeral(true).queue();
     }
 }
